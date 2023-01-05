165088a6c298daede983f424ebef53bc40a52595
kaufatim <fatimakauser42@gmail.com>
2023-01-05 13:20:04 -0800

    Update callingExternalRepository.java

--- callingExternalRepository.java
+++ callingExternalRepository.java
@@ -11,4 +11,6 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface callingExternalRepository extends JpaRepository<CallingExternalModel,String>{}
+public interface callingExternalRepository extends JpaRepository<CallingExternalModel,String>{}{
+  
+}
